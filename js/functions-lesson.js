7d47069dfbd3cfd7b496a0726bf37a8d2615bb56
Jason Crites <jcrites3@hotmail.com>
2022-11-21 12:52:46 -0800

    added function-lesson.html.js

--- js/functions-lesson.js
+++ js/functions-lesson.js
@@ -1,6 +1,12 @@
 //let userName = prompt("What is your name?");
 //let message = myFunction(userName);
-fight("Billy","Bob");
+let person1 = "Billy";
+    person2 = "Bob";
+fight(person1,person2);
+let x = isEven();
+console.log(x);
+let y = isEven();
+console.log(y);
 console.log(message);
 function myFunction(name){
    return "Thank you, " + name + ". You have ran myFunction";
