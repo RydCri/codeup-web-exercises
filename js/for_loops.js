54efe003d0b2f03b14e754d52d204496f0f8ba92
Jason Crites <jcrites3@hotmail.com>
2022-12-01 13:28:30 -0600

    created files for_loops.js/html - added for loops for exercises 1 and 2

---
+++ js/for_loops.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//function showMultiplicationTable(num) {
+//var mult = num;
+//for (var i = 1; i < 11; i++) {
+//    var result = mult * i;
+//console.log(mult + ' * ' + i + ' = ' + result);
+//}
+//}
+//var userPrompt = prompt(`Enter number for multiplication table~`);
+//showMultiplicationTable(userPrompt);
+
+//for (var i = 20; i <= 200; i++) {
+//    if (i % 2 === 0) {
+//        console.log(i + ' is even');
+//    } else {
+//        console.log(i + ' is odd');
+//    }
+//}
+
+
+//for (var i = 1; i < 11; i++) {
+//var result = ran + i;
+//  var ran = Math.floor(Math.random() * (200 - 20)) + 20;
+//     if(result % 2 == 0) {
+//     console.log(result + ' is even');
+//        } else {
+//        console.log(result + ' is odd');
+//     }
+//     }
+
+
