2c25a1db47c5d04837f4aad9d8190e5a6565e28d
Jason Crites <jcrites3@hotmail.com>
2022-12-08 11:31:59 -0600

    added number tower, experimented with ice cream in while.js

--- js/while.js
+++ js/while.js
@@ -1,42 +1,29 @@
-
-
-
-
-//var allCones = Math.floor(Math.random() * 50) + 50;
-//var sellCone = Math.floor(Math.random() * 5) + 1;
 //
-//for(i=allCones;i>0;i-=sellCone){
-//console.log(`${sellCone} cones sold. ${i} cones left.`)
-//}
-//if(i === 0){
-//  console.log(`Yay! I sold them all!`)
-//} else if(i < sellCone && i < 0) {
-//         console.log(`Cannot sell you ${sellCone} cones I only have ${i}...`)
-//}
-
 //var allCones = Math.floor(Math.random() * 50) + 50;
 //
-//for(i=allCones;i>0;i-=sellCone){
-//var sellCone = Math.floor(Math.random() * 5) + 1
-//console.log(`${i} cones left. buying ${sellCone}cones .`)
-//if(i === 0){
-//    console.log(`Yay! I sold them all!`)
-//} else if(i < sellCone) {
-//    console.log(`Cannot sell you ${sellCone} cones I only have ${i}...`)
-//}
-
-//`${} cones sold...`  // if there are enough cones
-//`Cannot sell you ${} cones I only have ${}...`  // If there are not enough cones
-//`Yay! I sold them all!` // If there are no more cones
-
-
-//let conesToSell = Math.floor(Math.random() * 50) + 50;
+//let i = allCones;
 //do {
-//    let conesToBuy = Math.floor(Math.random() * 5) + 1;
-//    if (conesToBuy <= conesToSell) {
-//    console.log(`${conesToBuy} cones sold.`)
-//    conesToSell -= conesToBuy;
-//    } else {
-//        console.log(`Cannot sell you ${conesToBuy} cones, I only have ${conesToSell}..`)
-//    }
-//} while (conesToSell > 0)
+//
+//var sellCone = Math.floor(Math.random() * 5) + 1;
+//if(i > sellCone) {
+//i -= sellCone
+//console.log(`${sellCone} cones sold. ${i} cones left.`)
+//}else if(i < sellCone && i >= 0){
+//console.log(`Cannot sell you ${sellCone} cones I only have ${i}...`);
+//}else{
+//console.log(`Yay! I sold them all!`)
+//}
+//} while (i < 0);
+//
+
+//let numMultiplyBy = 2;
+// let counter = 0;
+// while(counter < 16) {
+//     console.log(numMultiplyBy);
+//     numMultiplyBy *= 2;
+//     counter++;
+// }
+//for (let i = 0, num = 2; i < 16; i++) {
+//    console.log(num);
+//    num *= 2;
+//}
